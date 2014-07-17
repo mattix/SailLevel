697964935905f825327af8e7a3851f5e7c8ca552
Matti Ahinko <matti.ahinko@gmail.com>
2014-07-17 12:15:39 +0300

    Add calibrate string to translation

--- translations/SailLevel.ts
+++ translations/SailLevel.ts
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0">
+<context>
+    <name>LevelPage</name>
+    <message>
+        <source>Calibrate</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
